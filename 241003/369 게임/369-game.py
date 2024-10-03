73624ab068064a9a267e7f6e035fbbad7b1dc1df
y_gmltmd <50705621+heeseungy@users.noreply.github.com>
2024-10-03 22:37:58 +0900

    Title: 369 게임, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 241003/369 게임/369-game.py
+++ 241003/369 게임/369-game.py
@@ -8,7 +8,7 @@ while num <= n:
     # 숫자에 3, 6, 9중 하나가 들어가는가? = 1의 자리와 10의 자리에 3, 6, 9가 들어가는가?
     elif (num % 10) in (3, 6, 9):
         print(0, end=' ')
-    elif (num / 10) in (3, 6, 9):
+    elif (num // 10) in (3, 6, 9):
         print(0, end=' ')
     else:
         print(num, end=' ')
