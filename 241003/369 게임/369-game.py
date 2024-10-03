912a908ed05f6d9e768331fdde71313325071df3
y_gmltmd <50705621+heeseungy@users.noreply.github.com>
2024-10-03 22:34:00 +0900

    Title: 369 게임, Time: 98ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241003/369 게임/369-game.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+num = 1
+while num <= n:
+    # 3의 배수인가?
+    if num % 3 == 0:
+        print(0, end=' ')
+    # 숫자에 3, 6, 9중 하나가 들어가는가?
+    elif num % 10 in (3, 6, 9):
+        print(0, end=' ')
+    else:
+        print(num, end=' ')
+    num += 1
