f48b04048474e0249eebc61b8876bbd9308b739b
y_gmltmd <50705621+heeseungy@users.noreply.github.com>
2024-10-10 18:10:51 +0900

    Title: 알파벳 출력하기, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241010/알파벳 출력하기/print-alphabet.py
+++ 241010/알파벳 출력하기/print-alphabet.py
@@ -6,6 +6,6 @@ for i in range(1, n+1):
     for j in range(i):
         print(chr(alpha), end='')
         alpha += 1
-        if alpha == 79:
+        if alpha == 92:
             alpha = 65
     print()
