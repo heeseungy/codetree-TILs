4fcc14c2e8c5d7c9aa71ecfde8bd64060000b4c2
y_gmltmd <50705621+heeseungy@users.noreply.github.com>
2024-10-14 22:44:18 +0900

    Title: 특정 문자의 등장 횟수, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 241014/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
+++ 241014/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
@@ -1,5 +1,11 @@
 string = input()
-ee = string.count('ee')
-eb = string.count('eb')
+
+ee, eb = 0, 0
+
+for i in range(0, len(string) - 1, 1):
+    if string[i:i+2] == 'ee':
+        ee += 1
+    if string[i:i+2] == 'eb':
+        eb += 1
 
 print(f"{ee} {eb}")
