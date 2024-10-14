e5d2ba38e53291695bd7f226da003f1b69167859
y_gmltmd <50705621+heeseungy@users.noreply.github.com>
2024-10-15 03:30:52 +0900

    Title: 블럭쌓는 명령2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241015/블럭쌓는 명령2/block-stacking-commands2.py
+++ 241015/블럭쌓는 명령2/block-stacking-commands2.py
@@ -1,9 +1,9 @@
 n, k = map(int, input().split())
-arr = [0] * n
+arr = [0] * n + 1
 
 for _ in range(k):
     start, end = map(int, input().split())
-    for i in range(start, end):
+    for i in range(start, end+1):
         arr[i] += 1
 
 print(max(arr))
