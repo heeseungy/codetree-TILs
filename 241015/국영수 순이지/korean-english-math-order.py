43715dab06cd6e5b8318f95cf74bcc420d9386e8
y_gmltmd <50705621+heeseungy@users.noreply.github.com>
2024-10-15 02:45:52 +0900

    Title: 국영수 순이지, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 241015/국영수 순이지/korean-english-math-order.py
+++ 241015/국영수 순이지/korean-english-math-order.py
@@ -1,7 +1,7 @@
 n = int(input())
 students = [list(map(str, input().split())) for _ in range(n)]
 
-students.sort(key=lambda x: (x[1], x[2], x[3]), reverse=True, reverse=True, reverse=True)
+students.sort(key=lambda x: (int(x[1]), int(x[2]), int(x[3])), reverse=True)
 
 for s in students:
     print(*s)
