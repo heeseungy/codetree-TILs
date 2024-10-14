0307bcbc95abacf0d53b608ca6249bbf3fab3790
y_gmltmd <50705621+heeseungy@users.noreply.github.com>
2024-10-15 00:49:55 +0900

    Title: 문자열 돌리기, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 241015/문자열 돌리기/SPin-SPring.py
+++ 241015/문자열 돌리기/SPin-SPring.py
@@ -1,10 +1,4 @@
 s = input()
 
-print(s)
-for i in range(1, len(s) + 1):
-    print(s[:-i] + s[i])
-
-# 12345
-# 51234
-# 45123
-# 34512
+for i in range(len(s) + 1, 0, -1):
+    print(s[i-1:] + s[:i-1])
