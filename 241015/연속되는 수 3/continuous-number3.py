af5331e0d976bb31ae9e3a1ef55feeac8abc820f
y_gmltmd <50705621+heeseungy@users.noreply.github.com>
2024-10-15 14:24:31 +0900

    Title: 연속되는 수 3, Time: 137ms, Memory: 25MB, Status: Passed - CodeTree

--- 241015/연속되는 수 3/continuous-number3.py
+++ 241015/연속되는 수 3/continuous-number3.py
@@ -4,27 +4,13 @@ arr = []
 for _ in range(n):
     arr.append(int(input()))
 
-current_cnt = 1
-max_cnt = 1
+ans, cnt = 0, 0
+for i in range(n):
+	if i >= 1 and arr[i] * arr[i - 1] > 0:
+		cnt += 1
+	else:
+		cnt = 1
+	
+	ans = max(ans, cnt)
 
-sign = ''
-if arr[0] > 0:
-    sign = 'plus'
-else:
-    sigin = 'minus'
-
-for i in range(1, n):
-    if sign == 'plus' and arr[i] > 0:
-        current_cnt += 1
-        max_cnt = max(max_cnt, current_cnt)
-    elif sign == 'minus' and arr[i] < 0:
-        current_cnt += 1
-        max_cnt = max(max_cnt, current_cnt)
-    elif sign == 'plus' and arr[i] < 0:
-        current_cnt = 1
-        sign = 'minus'
-    else:
-        current_cnt = 1
-        sign = 'plus'
-
-print(max_cnt)
+print(ans)
