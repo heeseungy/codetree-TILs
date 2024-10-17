8deb43f108418fd770e91ede0d83f6cf5333dc9c
y_gmltmd <50705621+heeseungy@users.noreply.github.com>
2024-10-17 20:54:57 +0900

    Title: 방향에 맞춰 이동, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 241017/방향에 맞춰 이동/move-in-direction.py
+++ 241017/방향에 맞춰 이동/move-in-direction.py
@@ -1,24 +1,25 @@
-n = int(input())
+# (0, 0)에서 출발
+
+# N번 이동
+
+# N번에 걸쳐 움직이는 방향, 이동거리가 주어짐
+
+# 최종위치는?
 
 x, y = 0, 0
 
-# 우, 하, 좌, 상
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
-
+n = int(input())
 for _ in range(n):
-    d, m = map(str, input().split())
-    if d == 'E':
-        x += dx[0]
-        y += dy[0]
+    d, m = input().split()
+    m = int(m)
+
+    if d == 'N':
+        y += m
+    elif d == 'E':
+        x += m
     elif d == 'S':
-        x += dx[1]
-        y += dy[1]
-    
-    elif d == 'W':
-        x += dx[2]
-        y += dy[2]
-    
-    elif d == 'N':
-        x += dx[3]
-        y += dy[3]
+        y -= m
+    else:
+        x -= m
+
+print(x, y)
