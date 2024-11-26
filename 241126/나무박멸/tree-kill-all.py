34eaeb055e932e0e87c00104f7705a45499d12a4
y_gmltmd <50705621+heeseungy@users.noreply.github.com>
2024-11-26 17:56:57 +0900

    Title: 나무박멸, Time: 280ms, Memory: 23MB, Status: Passed - CodeTree

--- 241126/나무박멸/tree-kill-all.py
+++ 241126/나무박멸/tree-kill-all.py
@@ -1,16 +1,20 @@
+#제초제 유효기간 감소 함수
 def remove():
     for y in range(n):
         for x in range(n):
             if time_graph[y][x] > 0:
                 time_graph[y][x] -= 1
 
+# 성장
 def growth():
+
+    # dx, dy 상하좌우
     dy = [-1, 1, 0, 0]
     dx = [0, 0, -1, 1]
-    temp_graph = [row[:] for row in graph]
 
     for y in range(n):
         for x in range(n):
+            # 나무가 있을 경우, 주변 인접나무 찾기
             if graph[y][x] > 0:
                 count = 0
                 for d in range(4):
@@ -19,18 +23,20 @@ def growth():
                     if 0 <= nx < n and 0 <= ny < n:
                         if graph[ny][nx] > 0:
                             count += 1
-                temp_graph[y][x] += count
-    for y in range(n):
-        for x in range(n):
-            graph[y][x] = temp_graph[y][x]
 
+                # 주변 인접나무만큼 성장하기
+                graph[y][x] += count
+
+# 번식
 def breeding():
     breeding_graph = [list(0 for _ in range(n)) for _ in range(n)]
+
     dy = [-1, 1, 0, 0]
     dx = [0, 0, -1, 1]
 
     for y in range(n):
         for x in range(n):
+            # 번식 가능한 칸 확인
             if graph[y][x] > 0:
                 count = 0
                 for d in range(4):
@@ -39,18 +45,20 @@ def breeding():
                     if 0 <= nx < n and 0 <= ny < n:
                         if graph[ny][nx] == 0 and time_graph[ny][nx] == 0:
                             count += 1
-                if count > 0:
-                    spread_amount = graph[y][x] // count
-                    for d in range(4):
-                        nx = x + dx[d]
-                        ny = y + dy[d]
-                        if 0 <= nx < n and 0 <= ny < n:
-                            if graph[ny][nx] == 0 and time_graph[ny][nx] == 0:
-                                breeding_graph[ny][nx] += spread_amount
+                # 찐번식할 나무 수 임시배열 옮기기
+                for d in range(4):
+                    nx = x + dx[d]
+                    ny = y + dy[d]
+                    if 0 <= nx < n and 0 <= ny < n:
+                        if graph[ny][nx] == 0 and time_graph[ny][nx] == 0:
+                            breeding_graph[ny][nx] += (graph[y][x] // count)
+    # 찐번식
     for y in range(n):
         for x in range(n):
             graph[y][x] += breeding_graph[y][x]
 
+
+# 제초제 살포
 def spraying():
     dy = [-1, -1, 1, 1]
     dx = [-1, 1, -1, 1]
@@ -105,14 +113,14 @@ def spraying():
     return total_killed
 
 n, m, k, c = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(n)]
-time_graph = [list(0 for _ in range(n)) for _ in range(n)]
-answer = 0
+graph = [list(map(int, input().split())) for _ in range(n)]  # 나무, 벽 입력 그래프
+time_graph = [list(0 for _ in range(n)) for _ in range(n)]  # 제초제 유효기간 그래프
+answer = 0  # m년동안 죽인 전체 나무 수
 
 for year in range(m):
-    remove()    # 제초제 지속 시간 감소
-    growth()    # 나무 성장
-    breeding()  # 나무 번식
+    remove()    # 제초제 유효기간 감소
+    growth()    # 성장
+    breeding()  # 번식
     answer += spraying()  # 제초제 살포
 
 print(answer)
